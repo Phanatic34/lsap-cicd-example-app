2f85283a310eb411b38bbd5565a164450ec2c542
Phanatic34 <0818philip@gmail.com>
2025-11-24 04:54:43 +0000

    add /health endpoint

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// app.js
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send("<h1>Welcome to the CI/CD Workshop!</h1>");
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
 });
-
-module.exports = app;
