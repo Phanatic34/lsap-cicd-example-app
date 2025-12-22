c19a4542d54a1d219a31695797eb84481871aa24
Phanatic34 <0818philip@gmail.com>
2025-12-22 21:51:52 +0800

    Update welcome message to v2

--- app.test.js
+++ app.test.js
@@ -22,7 +22,7 @@ describe("GET /", () => {
   it("should return a welcome message", async () => {
     const res = await request(server).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain("Welcome DEV demo v1!");
+    expect(res.text).toContain("Welcome DEV demo v2!");
   });
 });
 
